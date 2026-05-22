254dcdc2d61e6e175951903f29cab1307fdbd15f
GabrieleTurchetti <gabriele.turchetti02@gmail.com>
2026-05-22 20:15:11 +0200

    Added correct debugging and logging mode for I2S mic

--- components/audio_hal/i2s_mic.c
+++ components/audio_hal/i2s_mic.c
@@ -15,11 +15,10 @@ static i2s_chan_handle_t rx_handle;
 extern QueueHandle_t audio_ai_queue;
 
 void audio_hal_mic_init(void) {
-    // TODO: Add test flag
-    if (true) {
+    #ifdef CONFIG_PRIVACY_SHIELD_BUILD_DEBUG && CONFIG_PRIVACY_SHIELD_LOG_AUDIO
         // Boost USB baud rate for raw sample streaming
         uart_set_baudrate(UART_NUM_0, 2000000);
-    }
+    #endif
     
     ESP_LOGI(TAG, "Initializing I2S microphone hardware...");
     i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
@@ -64,8 +63,7 @@ void audio_hal_mic_read_task(void *pvParameters) {
         esp_err_t err = i2s_channel_read(rx_handle, raw_samples, sizeof(raw_samples), &bytes_read, portMAX_DELAY);
         
         if (err == ESP_OK && bytes_read > 0) {
-            // TODO: Add test flag
-            if (true) {
+            #ifdef CONFIG_PRIVACY_SHIELD_BUILD_DEBUG && CONFIG_PRIVACY_SHIELD_LOG_AUDIO
                 int64_t current_time = esp_timer_get_time();
                 int delta_ms = (current_time - last_read_time) / 1000; 
                 last_read_time = current_time;
@@ -78,25 +76,24 @@ void audio_hal_mic_read_task(void *pvParameters) {
                     } else {
                         // This prevents serial buffer overflow and CPU stalling
                         if (packet_count % 50 == 0) {
-                            ESP_LOGI(TAG, "[Pkg: %d] System stable. Frame ready in: %d ms", packet_count, delta_ms);
+                            ESP_LOGD(TAG, "[Pkg: %d] System stable. Frame ready in: %d ms", packet_count, delta_ms);
                         }
                     }
                 } else if (packet_count == 2000) {
                     // Final validation message after ~1 minute of continuous operation
-                    ESP_LOGI(TAG, "1 minute test completed! No underruns detected.");
+                    ESP_LOGD(TAG, "1 minute test completed! No underruns detected.");
                 }
-            }       
+            #endif       
 
             int samples_read = bytes_read / 4; // 4 bytes per 32-bit sample
 
             // Audio streaming phase
-            // TODO: Add test flag
-            if (true) {
+            #ifdef CONFIG_PRIVACY_SHIELD_BUILD_DEBUG && CONFIG_PRIVACY_SHIELD_LOG_AUDIO
                 for (int i = 0; i < samples_read; i++) {
                     // Apply offset correction and print to serial
-                    //printf("%ld\n", (raw_samples[i] >> 16) - dc_offset);
+                    printf("%ld\n", (raw_samples[i] >> 16) - dc_offset);
                 }
-            }
+            #endif
             
             // Convert 32-bit I2S data to 16-bit standard audio for the AI
             for (int i = 0; i < samples_read; i++) {
